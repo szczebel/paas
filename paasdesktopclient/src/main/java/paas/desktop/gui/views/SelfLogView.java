89cb61425744c89c76d75e015170e94943a20e50
Adam_Szczeblewski <Adam_Szczeblewski@epam.com>
2017-04-18 13:37:06 +0200

    fix for initial size being too wide (was as wide as longest string in selflog)

--- paasdesktopclient/src/main/java/paas/desktop/gui/views/SelfLogView.java
+++ paasdesktopclient/src/main/java/paas/desktop/gui/views/SelfLogView.java
@@ -18,9 +18,9 @@ public class SelfLogView extends LazyInitRichAbstractView {
     @Override
     protected JComponent wireAndLayout() {
         RollingConsole rollingConsole = new RollingConsole(1000);
-        //todo: doesn;t work, packed size too wide
-        rollingConsole.getComponent().setPreferredSize(new Dimension(800, 600));
         sysoutInterceptor.registerSwingConsumer(rollingConsole::append);
-        return rollingConsole.getComponent();
+        JComponent component = rollingConsole.getComponent();
+        component.setPreferredSize(new Dimension(800, 600));
+        return component;
     }
 }
